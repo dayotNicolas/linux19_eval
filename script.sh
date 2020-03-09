a970f7412aa749fc5482083670c4ce2acc4d011b
nicolas.dayot@ynov.com <nicolas.dayot@ynov.com>
2020-03-09 12:22:08 +0100

    mofifiction version 4 09_03_2020

--- script.sh
+++ script.sh
@@ -1,26 +1,46 @@
 #!/bin/bash
+codeerreur22='code erreur 22: fichier en cours d execution'
+codeerreur12='code erreur 12: impossible d effectuer l operation'
+currcodeerreur=''
 if [ -e /tmp/out/lock ]
 then
-    echo 'code erreur 22: script en cours d execution'
-    exit 2>>/tmp/out/log
+    currcodeerreur=$codeerreur22
+    echo $currcodeerreur
+    echo $currcodeerreur >> /tmp/out/log
+    exit 22
 else
     if [ -d /tmp/in ]
     then
         touch /tmp/out/lock
-        for file in /tmp/in/*
+        for file in $(find /tmp/in -name '*.txt')
         do
-            if [ -e $file ] && [ -f $file ]
+            name=${file##*/}
+            if [ -f $file ]
             then
-                echo 'OK!'
-                cp /tmp/in/* /tmp/out
-                chmod 775 /tmp/out
-                gzip /tmp/out/*
-                rm /tmp/out/lock.gz
+                if [ ! -e "/tmp/out/$name.gz" ]
+                then
+                    echo $file >> /tmp/out/log
+                    echo 'OK!'
+                    cp /tmp/in/* /tmp/out
+                    chmod 775 /tmp/out
+                    gzip /tmp/out/*
+                    echo "/tmp/out/$name.gz traité" >> /tmp/out/log
+                    find /tmp/out -name '*.txt' | xargs rm -f
+                else
+                    continue
+                fi
             else
-                echo  'code erreur 12: impossible d effectuer l operation'
-                exit 2>>/tmp/out/log
+                currcodeerreur=$codeerreur12
+                echo $currcodeerreur
+                echo $currcodeerreur >> /tmp/out/log
+                exit 12
             fi
         done
+        rm /tmp/out/lock.gz
+        if [ -e /tmp/out/log.gz ]
+        then
+            rm /tmp/out/log.gz
+        fi
     else
     exit 2
     fi
